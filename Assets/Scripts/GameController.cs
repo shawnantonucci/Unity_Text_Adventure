9bf1229428a70d60362b39da169856edd7253c09
shawnantonucci <shawnantonucci2018@gmail.com>
2019-10-25 15:17:54 -0700

    added scriptable objects
    added room object
    gamecontroller updating text on screen
    room navigation getting current room

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text displayText;
+
+    [HideInInspector] public RoomNavigation roomNavigation;
+
+    List<string> actionLog = new List<string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        roomNavigation = GetComponent<RoomNavigation>();
+    }
+
+    void Start()
+    {
+        DisplayRoomText();
+        DisplayLoggedText();
+    }
+
+    public void DisplayLoggedText()
+    {
+        string logAsText = string.Join("\n", actionLog.ToArray());
+
+        displayText.text = logAsText;
+    }
+
+    public void DisplayRoomText()
+    {
+        string combinedText = roomNavigation.currentRoom.description + "\n";
+
+        LogStringWithReturn(combinedText);
+    }
+
+    public void LogStringWithReturn(string stringToAdd)
+    {
+        actionLog.Add(stringToAdd + "\n");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomNavigation.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomNavigation : MonoBehaviour
+{
+
+    public Room currentRoom;
+
+}
