0311286884638d6e7c464cf4a4c0a0bbf410ffc1
shawnantonucci <shawnantonucci2018@gmail.com>
2019-10-25 15:56:43 -0700

    accepting input and returning that to the screen

---
+++ Assets/Scripts/TextInput.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextInput : MonoBehaviour
+{
+    public InputField inputField;
+
+    GameController controller;
+
+    void Awake()
+    {
+        controller = GetComponent<GameController>();
+        inputField.onEndEdit.AddListener(AcceptStringInput);
+    }
+
+    void AcceptStringInput(string userInput)
+    {
+        userInput = userInput.ToLower();
+        controller.LogStringWithReturn(userInput);
+        InputComplete();
+    }
+
+    void InputComplete()
+    {
+        controller.DisplayLoggedText();
+        inputField.ActivateInputField();
+        inputField.text = null;
+    }
+}
