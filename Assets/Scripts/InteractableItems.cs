c125a18c67ab0f8a2e86ab13f43ba6834ef59c6f
shawnantonucci <shawnantonucci2018@gmail.com>
2019-10-25 21:24:15 -0700

    added use item to go to a secret room

--- Assets/Scripts/InteractableItems.cs
+++ Assets/Scripts/InteractableItems.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class InteractableItems : MonoBehaviour
 {
+    public List<InteractableObject> usableItemList;
+
     public Dictionary<string, string> examineDictionary = new Dictionary<string, string>();
     public Dictionary<string, string> takeDictionary = new Dictionary<string, string>();
 
     [HideInInspector] public List<string> nounsInRoom = new List<string>();
 
+    Dictionary<string, ActionResponse> useDictionary = new Dictionary<string, ActionResponse>();
     List<string> nounsInInventory = new List<string>();
     GameController controller;
 
@@ -30,6 +33,44 @@ public class InteractableItems : MonoBehaviour
         return null;
     }
 
+    public void AddActionResponsesToUseDictionary()
+    {
+        for (int i = 0; i < nounsInInventory.Count; i++)
+        {
+            string noun = nounsInInventory[i];
+
+            InteractableObject interactableObjectInInventory = GetInteractableObjectFromUsableList(noun);
+            if (interactableObjectInInventory == null)
+                continue;
+
+            for (int j = 0; j < interactableObjectInInventory.interactions.Length; j++)
+            {
+                Interaction interaction = interactableObjectInInventory.interactions[j];
+
+                if (interaction.actionResponse == null)
+                    continue;
+
+                if (!useDictionary.ContainsKey(noun))
+                {
+                    useDictionary.Add(noun, interaction.actionResponse);
+                }
+            }
+
+        }
+    }
+
+    InteractableObject GetInteractableObjectFromUsableList(string noun)
+    {
+        for (int i = 0; i < usableItemList.Count; i++)
+        {
+            if (usableItemList[i].noun == noun)
+            {
+                return usableItemList[i];
+            }
+        }
+        return null;
+    }
+
     public void DisplayInventory()
     {
         controller.LogStringWithReturn("You look in your backpack, inside you have: ");
@@ -47,12 +88,14 @@ public class InteractableItems : MonoBehaviour
         nounsInRoom.Clear();
     }
 
-    public Dictionary<string, string> Take (string[] seperatedInputWords)
+    public Dictionary<string, string> Take(string[] separatedInputWords)
     {
-        string noun = seperatedInputWords[1];
+        string noun = separatedInputWords[1];
+
         if (nounsInRoom.Contains(noun))
         {
             nounsInInventory.Add(noun);
+            AddActionResponsesToUseDictionary();
             nounsInRoom.Remove(noun);
             return takeDictionary;
         }
@@ -62,4 +105,30 @@ public class InteractableItems : MonoBehaviour
             return null;
         }
     }
+
+    public void UseItem(string[] separatedInputWords)
+    {
+        string nounToUse = separatedInputWords[1];
+
+        if (nounsInInventory.Contains(nounToUse))
+        {
+            if (useDictionary.ContainsKey(nounToUse))
+            {
+                bool actionResult = useDictionary[nounToUse].DoActionResponse(controller);
+                if (!actionResult)
+                {
+                    controller.LogStringWithReturn("Hmm. Nothing happens.");
+                }
+            }
+            else
+            {
+                controller.LogStringWithReturn("You can't use the " + nounToUse);
+            }
+        }
+        else
+        {
+            controller.LogStringWithReturn("There is no " + nounToUse + " in your inventory to use");
+        }
+    }
+
 }
